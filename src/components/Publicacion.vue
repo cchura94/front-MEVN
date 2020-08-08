681d2846fbc297541cd4352545cb7481f48c4a03
cchura94 <cchura.cpc@gmail.com>
2020-08-07 22:23:35 -0400

    axios

---
+++ src/components/Publicacion.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Publicación</h1>
+    <table border="1">
+      <tr>
+        <td>ID</td>
+        <td>TITULO</td>
+        <td>ESTADO</td>
+      </tr>
+      <tr v-for="(publi, index) in publicaciones" :key="index">
+        <td>{{ publi.id }}</td>
+        <td>{{ publi.title }}</td>
+        <td v-if="publi.completed" style="background-color:green">
+          {{ publi.completed }}
+        </td>
+        <td v-else style="background-color:red">{{ publi.completed }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      publicaciones: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos/")
+      .then((response) => {
+        console.log(response.data);
+        this.publicaciones = response.data;
+      });
+
+    /*fetch("https://jsonplaceholder.typicode.com/todos/")
+      .then(response => {
+        return response.json();
+      })
+      .then(json => {
+        console.log(json);
+        s.publicaciones = json;thi
+      });
+      */
+  },
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Publi></Publi>
   </div>
 </template>
+
+<script>
+import Publi from "./../components/Publicacion";
+
+export default {
+  components: {
+    Publi
+  }
+};
+</script>
